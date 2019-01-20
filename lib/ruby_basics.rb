15977dbacc44cf334adb1002ef0f636fd6c8854e
Igor Rud <rud.igor23@gmail.com>
2019-01-20 18:27:04 +0000

    Automatically backed up by Learn

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,13 +1,15 @@
-def division(num1, num2)
- 
+def division(num1 = 42, num2 = 7)
+ eq = num1/num2
 end
 
-def assign_variable(value)
-  
+def assign_variable(value = "Bob")
+"#{value}"
 end
 
-def argue
-  
+def argue(phrase)
+phrase = "I'm right and you are wrong!"
+puts phrase
+return phrase
 end
 
 def greeting
